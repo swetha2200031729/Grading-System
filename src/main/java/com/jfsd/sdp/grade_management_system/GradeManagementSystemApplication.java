ef68ff7ba2fbe61e3b93f766a77d0b18f0223497
swetha2200031729 <swevara@gmail.com>
2024-10-02 17:20:47 +0530

    my first commit

---
+++ src/main/java/com/jfsd/sdp/grade_management_system/GradeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jfsd.sdp.grade_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfsd/sdp/grade_management_system/GradeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfsd.sdp.grade_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
